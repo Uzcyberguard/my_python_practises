0b55933ed3c2f0bf7cb7caafb54954ccb387f36d
Hasan Normamatov <hasannormamatov696@gmail.com>
2025-06-14 19:21:24 +0500

    +
    +

---
+++ while1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 14 17:55:55 2025
+
+@author: User
+"""
+b=[]
+c=True
+while True :
+    a=str(input("yaxshi ko'rgan kitobingizni kiriting(dasturni to'xtatish uchun 'stop' yoke 'quit' deb yozing)\n>>>"))
+ 
+    if a=="quit" or a=="stop":
+        break
+    b.append(a)
+print(f"{b} bu sizning yaxshi ko'rgan kitoblariz😂😊")
